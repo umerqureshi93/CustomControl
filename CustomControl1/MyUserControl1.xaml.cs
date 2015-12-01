ecb75f99ce5c2a2a1fbe5636cace42ac78ea64fd
umerqureshi93 <umer_aries@live.com>
2015-11-30 16:34:12 -0800

    Custom Control

---
+++ CustomControl1/MyUserControl1.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace CustomControl1
+{
+    public sealed partial class MyUserControl1 : UserControl
+    {
+        public MyUserControl1()
+        {
+            this.InitializeComponent();
+        }
+
+
+
+
+        public string textblock
+        {
+            get { return (string)GetValue(textblockProperty); }
+            set { SetValue(textblockProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for textblock.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty textblockProperty =
+            DependencyProperty.Register("textblock", typeof(string), typeof(MyUserControl1), null);
+
+
+
+        public ImageSource ImageSource
+        {
+            get { return (ImageSource)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ImageSource.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImageSourceProperty =
+            DependencyProperty.Register("ImageSource", typeof(ImageSource), typeof(MyUserControl1), null);
+
+
+
+
+    }
+}
